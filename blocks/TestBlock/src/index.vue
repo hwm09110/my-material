fbb529b3c4f0615deb18e032a84a079f2121650a
hwm09110 <771734186@qq.com>
2021-03-25 10:48:32 +0800

    add new block

---
+++ blocks/TestBlock/src/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="example-block">
+    <h1>TestBlock</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestBlock',
+
+  data() {
+    return {}
+  },
+
+  created() {},
+
+  methods: {}
+}
+</script>
+
+<style>
+.example-block {
+}
+</style>
